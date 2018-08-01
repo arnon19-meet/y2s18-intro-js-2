ad5bbf5ac04b59fac2e93cc893af02611bcecf51
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 14:15:39 +0300

    g

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,9 @@
 // Create your 'me' object literal here!
+var me = {
+	firstName: "Arnon",
+	lastName: "Lutsky",
+	favoriteFoods: ["pasta","hamborger"],
+	age: 16,
+	picture: "https://media.overstockart.com/optimized/cache/data/product_images/KL1839-1000x1000.jpg",	
+}
+document.write(JSON.stringify(me))
