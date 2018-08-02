420372371d27cb5a54e30ab1ab664131be270427
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 16:37:29 +0300

    g

--- event-listeners/event-listeners.js
+++ event-listeners/event-listeners.js
@@ -1,13 +1,17 @@
 // YOUR TASK: Add more pictures!
-var pictures = ['./imgs/dog.jpg'];
+var pictures = ['./imgs/rain1.jpg','./imgs/rain2.jpg','./imgs/rain3.jpg','./imgs/rain4.jpg','./imgs/lightning.jpg'];
 var currentIndex = 0;
-
+document.getElementById("photo").addEventListener("click", function (event) {
+	showNextPicture(event.target);
+});
 function showNextPicture() {
   currentIndex++; // increment current picture
   // if currentIndex is too large, start from the beginning again
   if (currentIndex >= pictures.length) {
-    currentIndex = 0;
-  }
 
+    currentIndex = 0;
   // YOUR TASK: Finish this function!
+}
+var image = document.getElementById('photo');
+    image.src = pictures[currentIndex]
 }
--- html-events/html-events.js
+++ html-events/html-events.js
@@ -1,1 +1,10 @@
 // Put all your JavaScript in this file!
+var btn = document.getElementById("button")
+btn.style.height = "100px"
+btn.style.width = "200px"
+
+
+function Background(){
+	BGC=prompt("pick a color")
+	Backgroundcolor=BGC
+}
